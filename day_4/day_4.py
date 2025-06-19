187c6304ab85afb9e254fde12c39cfe9a2e12e2d
UP240233 <up240233@alumnos.upa.edu.mx>
2025-06-19 13:08:33 -0600

    Add files via upload

---
+++ day_4/day_4.py
@@ -0,0 +1,44 @@
+#1
+a =("Thirty")
+b =("Days")
+c =("Of")
+d =("Python")
+#2
+e =("Coding")
+f =("For")
+g =("All")
+#3
+line1 =(a+" "+b+" "+c+" "+d)
+line2 =(e+" "+f+" "+g)
+print(line1)
+print(line2)
+company = ("Coding For All")
+#4
+print(company)
+#5
+print("La longitud de la cadena company es ", len(company))
+#6
+print(company.upper())
+#7
+print(company.lower())
+#8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+#9
+s1 = "Coding For All"
+s2 = "Python for Everyone"
+result1 = s1[s1.find(" ") + 1:]
+print(result1)  # → "For All"
+#10
+contains_index = s1.find("Coding")    
+contains_bool  = "Coding" in s1      
+print(contains_index, contains_bool)
+#11
+result3 = s1.replace("Coding", "Python")
+print(result3)  
+#12 
+result4 = s2.replace("Everyone", "All")
+print(result4) 
+#13
+
